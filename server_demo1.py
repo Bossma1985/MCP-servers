fad761c26a2d5c71c90fc4dd105780e306e8bed6
Rafa Botella Martí <rafasimatero@gmail.com>
2025-05-12 08:14:20 +0200

    Añado conversor de celsius a farengeit

--- server_demo1.py
+++ server_demo1.py
@@ -6,3 +6,8 @@ mcp = FastMCP("Demo")
 def add(a: int, b: int) -> int:
     """Suma dos números enteros y devuelve el resultado"""
     return a + b
+
+@mcp.tool()
+def celcius_to_fahrenheit(c: float) -> float:
+    """Convierte grados Celcius a Fahrenheit"""
+    return c * 9/5 + 32
